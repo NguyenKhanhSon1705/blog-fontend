9f5a7ae1aab2333bdaf06c1d8fbc21fdb816052a
unknown <quyetthang.ads1112@gmail.com>
2024-11-14 20:26:11 +0700

    Sua lai reponsive Hero va Footer cho man hinh tablet

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import PopularPost from "./Popular/PopularPost";
 const Hero = () => {
   return (
     <div className="w-full bg-[#909090]">
-      <div className="w-full max-w-[1200px] py-4 px-3 mx-auto flex flex-col md:flex-row md:justify-between md:items-center gap-8 md:py-9">
+      <div className="w-full max-w-[1200px] py-4 px-3 mx-auto flex flex-col sm:flex-col lg:flex-row lg:justify-between lg:items-center gap-8 lg:py-9">
         <div className="flex flex-col gap-4 flex-1">
           <div className="flex items-center gap-2 ">
             <span className="text-white">Innovation</span>
--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import LOGO from "../assets/logo.png";
 const Footer = () => {
   return (
-    <div className="w-full max-w-[1200px] mx-auto">
-      <div className="flex p-2 flex-col md:flex-row gap-12 md:p-0">
+    <div className="w-full p-3 lg:max-w-[1200px] mx-auto lg:p-0">
+      <div className="flex p-2 flex-col md:flex-row gap-12 lg:p-2 xl:p-0">
         <div className="flex flex-col gap-1 w-full">
           <img className="h-[150px] object-contain" src={LOGO} alt="" />
-          <p>
+          <p className="text-center md:text-start">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
             sed do eiusmod.
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 text-center md:text-start w-full flex-3">
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 text-center md:text-start w-full flex-3">
             <div>
-                <h5 className="font-medium mb-4">Categories</h5>
+                <h5 className="font-medium mb-4 text-[20px]">Categories</h5>
                 <ul>
                     <li>International</li>
                     <li>Regional</li>
@@ -26,7 +26,7 @@ const Footer = () => {
                 </ul>
             </div>
             <div>
-                <h5 className="font-medium mb-4">Company</h5>
+                <h5 className="font-medium mb-4 text-[20px]">Company</h5>
                 <ul>
                     <li>About Us</li>
                     <li>Careers</li>
@@ -36,7 +36,7 @@ const Footer = () => {
                 </ul>
             </div>
             <div>
-                <h5 className="font-medium mb-4">Social Media</h5>
+                <h5 className="font-medium mb-4 text-[20px]">Social Media</h5>
                 <ul>
                     <li>Youtube</li>
                     <li>Instagram</li>
